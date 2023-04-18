e5dcea0b997914bed0aa88e6502a1ae710495cf4
Ryan Doerr <ryan.doerr15@gmail.com>
2023-04-18 17:56:36 -0400

    added functions

--- js/site.js
+++ js/site.js
@@ -70,7 +70,7 @@ function buildDropdown() {
 
     dropdownMenu.innerHTML = '';
 
-    let currentEvents = events; //TODO - get these from storage
+    let currentEvents = getEventData(); 
 
     let cityNames = currentEvents.map(
         function (event) {
@@ -79,12 +79,12 @@ function buildDropdown() {
     );
     //let cityNames = currentEvents.map(event => event.city);   **same as above
 
-    let citiesSet = new Set(cityNames);
+    let citiesSet = new Set(cityNames); //sets do not allow repeated values 
     let distinctCities = [...citiesSet]; //creates this: ['Charlotte', 'San Diego', 'New York']
 
     const dropdownTemplate = document.getElementById('dropdownItemTemplate');
 
-    //copy the template
+    //copy the template, importNode allows us to make that copy
     let dropdownItemNode = document.importNode(dropdownTemplate.content, true);
 
     //make our changes
@@ -110,6 +110,8 @@ function buildDropdown() {
     }
 
     displayEventData(currentEvents);
+    displayStats(currentEvents);
+    document.getElementById('location').innerText = 'All Events';
 }
 
 function displayEventData(currentEvents) {
@@ -126,9 +128,138 @@ function displayEventData(currentEvents) {
         tableRow.querySelector('[data-id="event"]').textContent = event.event;
         tableRow.querySelector('[data-id="city"]').textContent = event.city;
         tableRow.querySelector('[data-id="state"]').textContent = event.state;
-        tableRow.querySelector('[data-id="attendance"]').textContent = event.attendance;
-        tableRow.querySelector('[data-id="date"]').textContent = event.date;
+        tableRow.querySelector('[data-id="attendance"]').textContent = event.attendance.toLocaleString();
+        tableRow.querySelector('[data-id="date"]').textContent = new Date(event.date).toLocaleDateString();
   
         eventTable.appendChild(tableRow);
     }
+}
+
+function calculateStats(currentEvents) {
+    let total = 0;
+    let avg = 0;
+    let most = 0;
+    let least = currentEvents[0].attendance;
+
+    for (let i = 0; i < currentEvents.length; i++) {
+        let currentAttendance = currentEvents[i].attendance;
+
+        total += currentAttendance;
+
+        if (currentAttendance > most) {
+          most = currentAttendance;
+        }
+
+        if (currentAttendance < least) {
+          least = currentAttendance;
+        }
+    }
+    avg = total / currentEvents.length;
+
+    let stats = {
+      total: total,
+      average: avg,
+      most: most,
+      least: least
+    }
+
+    return stats;
+}
+
+function displayStats(currentEvents) {
+    let statistics = calculateStats(currentEvents);
+
+    //get the elements where the stats go
+    //set their text to be the correct stat from statistics
+    document.getElementById('total').textContent = statistics.total.toLocaleString();
+
+   //Math.round rounds up or down to a whole number, toLocalString() removes decimals and adds commas
+    document.getElementById('average').textContent = Math.round(statistics.average).toLocaleString(); 
+
+    document.getElementById('most').textContent = statistics.most.toLocaleString();
+    document.getElementById('least').textContent = statistics.least.toLocaleString();
+}
+
+function getEventData() {
+    let data = localStorage.getItem('rdSuperDogEventData2023');
+
+    if (data == null) {
+        localStorage.setItem('rdSuperDogEventData2023', JSON.stringify(events));   //JSON = javascript object notation
+    }
+
+    let currentEvents = data == null ? events : JSON.parse(data); 
+
+    // if statement below is same as turnary statement above
+    // if (data == null) {
+    //  currentEvents = events;
+    //} else {
+    //  currentEvents = JSON.parse(data);
+    //}
+
+    return currentEvents;
+}     
+
+function viewFilteredEvents(dropdownItem) {
+    let cityName = dropdownItem.getAttribute('data-string');
+
+    //get all my events
+    let allEvents = getEventData();
+
+    if (cityName == 'All') {
+        displayEventData(allEvents);
+        document.getElementById('location').innerText = 'All Events';
+    
+        return; //return in this instance will end the function if cityName does not equal All
+      }  
+
+      //filter those events to just the selected city
+      let filteredEvents = allEvents.filter(event => event.city.toLowerCase() == cityName.toLowerCase());
+      // let filteredEvents = allEvents.filter(
+      //    function (event)) {
+      //    return event.city.toLowerCase() == cityName.toLowerCase();
+      //}
+
+      //display the stats for those events
+      displayStats(filteredEvents);
+
+      //change the stats header
+      document.getElementById('location').innerText = cityName;
+
+      //display only those events in the table
+      displayEventData(filteredEvents);
+    
+}
+
+function saveNewEvent() {
+    //get the form input values
+    let name = document.getElementById('newEventName').value;
+    let city = document.getElementById('newEventCity').value;
+
+    let stateSelect = document.getElementById('newEventState');
+    let stateIndex = stateSelect.selectedIndex;
+    let state = stateSelect.options[stateIndex].text;
+
+    let attendance = parseInt(document.getElementById('newEventAttendance').value, 10);
+
+    let dateValue = document.getElementById("newEventDate").value;
+    dateValue = new Date(dateValue);
+    let date = dateValue.toLocaleDateString();
+
+    //create a new event object
+    let newEvent = {
+      event: name,
+      city: city,
+      state: state,
+      attendance: attendance,
+      date: date,
+    };
+
+    //add it to the array of current events
+    let events = getEventData();
+    events.push(newEvent);
+
+    //save the array with the new event
+    localStorage.setItem('rdSuperDogEventData2023', JSON.stringify(events));
+
+    buildDropdown();
 }
